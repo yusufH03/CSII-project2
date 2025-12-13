d2f1c40c4cd0b69a4205d62c150ef1b02c75486e
yusufH03 <03eaglesoccer@gmail.com>
2025-12-13 14:19:03 -0600

    completed code with debugging tools

--- maze_logic.py
+++ maze_logic.py
@@ -1,12 +1,11 @@
-# T
-# CSCI-1620: Final Project-Part 1
+# This file contains all the logic for the Maze Solver app. Two Enum classes are used, Directions & Facing, which have collections of steps to solve the maze and facing directions respectively. The Logic class contains methods that handle: converting the GUI to an array, solving the maze, and changing the GUI based on user clicks. Helper methods check the next location in the maze and rotate the direction being faced. Error handling is used to ensure that the maze is solvable. The user creates a maze by clicking cells within the given grid. Once complete, the user clicks the  submit button and receives directions to solve their maze.
+# CSCI-1620: Final Project-Part 2
 # Written By: Yusuf Hussain, 12/9/2025
-# import everything that is needed (PyQt6, the GUI file, CSV module, and Regular Expressions module)
-
+# import everything that is needed (PyQt6, the GUI file, and Enum)
 from PyQt6.QtWidgets import *
-
 from gui import *
 from enum import Enum
+
 
 class Directions(Enum):
     """
@@ -16,6 +15,7 @@ class Directions(Enum):
     right = 1
     left = -1
 
+
 class Facing(Enum):
     """
     Enum class for the facing directions while in the maze.
@@ -24,6 +24,7 @@ class Facing(Enum):
     east = 1
     south = 2
     west = 3
+
 
 class Logic(QMainWindow, Ui_MazeSolver):
     def __init__(self):
@@ -42,9 +43,7 @@ class Logic(QMainWindow, Ui_MazeSolver):
             [0, 1, 1, 1, 1, 1, 1, 0, ],
             [0, 0, 0, 0, 0, 0, 2, 0, ]
         ]
-        # class variable - store all the base directions
-        self.directions = ["forward", "backward", "right", "left"]
-        # class variable - store all the buttons in a list
+        # class variable - store all the buttons (maze cells) in a list
         self.editable_maze = [
             [self.c2_2, self.c2_3, self.c2_4, self.c2_5, self.c2_6, self.c2_7],
             [self.c3_2, self.c3_3, self.c3_4, self.c3_5, self.c3_6, self.c3_7],
@@ -53,7 +52,7 @@ class Logic(QMainWindow, Ui_MazeSolver):
             [self.c6_2, self.c6_3, self.c6_4, self.c6_5, self.c6_6, self.c6_7],
             [self.c7_2, self.c7_3, self.c7_4, self.c7_5, self.c7_6, self.c7_7],
         ]
-        # class variable - directions that will be displayed
+        # class variable - directions that will be displayed once the maze is solved
         #self.instructions = ""
 
         # call the toggle_color method when a button/cell is clicked
@@ -62,8 +61,7 @@ class Logic(QMainWindow, Ui_MazeSolver):
                 btn.clicked.connect(lambda _, b=btn: self.toggle_color(b))
         self.submit_button.clicked.connect(self.convert)
 
-
-    def toggle_color(self, button:QPushButton):
+    def toggle_color(self, button: QPushButton) -> None:
         """
         Toggle the background color of a button between default and wall colors.
         :param button: QPushButton to be changed
@@ -72,155 +70,122 @@ class Logic(QMainWindow, Ui_MazeSolver):
         if "rgb(0, 85, 0)" in button.styleSheet():
             button.setStyleSheet("")  # turn a green/wall space white
         else:
-            button.setStyleSheet("background-color: rgb(0, 85, 0);") # turn a white/open space green
+            button.setStyleSheet("background-color: rgb(0, 85, 0);")  # turn a white/open space green
 
-    # convert maze to an array
-    def convert(self):
+    def convert(self) -> None:
         """
         Reads the location of all open space and walls in the GUI maze and creates a list variable that matches it.
-        :return:
+        :return: None
         """
         raw_maze = []
         for r in range(8):
             row_copy = []
             for c in range(8):
-                row_copy.append(self.default_maze[r][c])
+                row_copy.append(self.default_maze[r][c]) #copy default_maze to raw_maze
             raw_maze.append(row_copy)
 
+        # iterate through the editable part of the maze (inner 6x6)
         for r in range(6):
             for c in range(6):
                 button = self.editable_maze[r][c]
                 styles = button.styleSheet()
                 if "rgb(0, 85, 0)" in styles:
-                    status = 0
+                    status = 0 # set the status to be a wall if it's a wall in the GUI
                 else:
-                    status = 1
-                raw_maze[r+1][c+1] = status
+                    status = 1 # set the status as open if it's not a wall
+                raw_maze[r + 1][c + 1] = status # update raw_maze
         # remove later########################################
         for row in raw_maze:
             print(row)
 
-        #self.solve_maze(Facing.south, raw_maze)  # pass the converted maze into the method that solves it
         try:
-            print(raw_maze[0][1])
-            print(raw_maze[7][6])
-            ret = [d.name for d in self.solve_maze(Facing.south, raw_maze) [0][::-1]]
-            print(1, ret)
+            how_to_solve_r = [d.name for d in self.solve_maze(Facing.south, raw_maze)[0][::-1]]
+            print(how_to_solve_r)
+            how_to_solve = "How to solve your maze:"
+            for d in how_to_solve_r:
+                if d == "forward":
+                    how_to_solve += f" Move forward."
+                elif d == "right":
+                    how_to_solve += f" Turn right."
+                elif d == "left":
+                    how_to_solve += f" Turn left."
+            how_to_solve += ". Congratulations, you made it out!"
+            self.output_label.setText(how_to_solve) # tell the user how to solve the maze
         except Exception as e:
             print(e)
+            self.output_label.setText("Maze is unsolvable. Please create a new maze and try again.")
 
-    # solve the maze
-    def solve_maze(self, facing:Facing, raw_maze:list, x:int=0, y:int=1) -> tuple[list[Directions], bool]:
+    def solve_maze(self, facing: Facing, raw_maze: list, x: int = 0, y: int = 1) -> tuple[list[Directions], bool]:
         """
-
+        Solves the maze if it is solvable. Starting position is always (0,1); user can't change it.
         :param x: starting x position
         :param y: starting y position
         :param raw_maze: current maze layout in a list format
         :param facing: current facing direction (N,S,E,W)
-        :return: list of directions, if directions are valid
+        :return: list of directions to the current x,y in reverse order; if directions are valid
         """
-        # start will always be at the top left (1,2)
-        status = ' '
-        #current_directions = self.directions  # list with the most current directions
-        print(f"{x},{y}")
+        print(f"{x},{y}") #remove
         # check base cases
-        if raw_maze[x][y] == 0:
-            #status = 'wall'
+        if raw_maze[x][y] == 0: #wall
             return [], False
-        elif raw_maze[x][y] == 2:
-            #status = 'end'
+        elif raw_maze[x][y] == 2: #end of maze
             return [], True
-        elif raw_maze[x][y] == 3:
-            #status = 'visited'
+        elif raw_maze[x][y] == 3: #visited
             return [], False
-        self.output_label.setText(f"Currently in cell {x}, {y}")
         # mark which are visited
         raw_maze[x][y] = 3
 
-        current_directions = [Directions.forward] # the last step in solving the maze is always going forward
+        current_directions = [Directions.forward]  #last step in solving the maze is always going forward
+        new_x, new_y = self.calculate_coords(facing, x, y) #next coordinates to move to
+        directions, success = self.solve_maze(facing, raw_maze, new_x, new_y) #update the most current directions and their success to variables
 
-        new_x, new_y = self.calculate_index(facing, x, y)
-        directions, success = self.solve_maze(facing, raw_maze, new_x, new_y)
-
-        #
+        # Always move forward if possible, otherwise rotate clockwise once.
         if success:
-            directions.extend(current_directions) # for element in current_directions, append directions
+            directions.extend(current_directions)  #for item in current_directions, append directions
             return directions, success
         else:
-            facing = self.rotate(facing)
-            new_x, new_y = self.calculate_index(facing, x, y)
-            directions, success = self.solve_maze(facing, raw_maze, new_x, new_y)
+            facing = self.rotate(facing) #rotate right
+            new_x, new_y = self.calculate_coords(facing, x, y) #next coordinates
+            directions, success = self.solve_maze(facing, raw_maze, new_x, new_y) #update vars
+            # Move forward after rotating right if possible, otherwise rotate counterclockwise once.
             if success:
-                current_directions.append(Directions.right)
-                directions.extend(current_directions)
+                current_directions.append(Directions.right) #turn right (clockwise)
+                directions.extend(current_directions) #add a right turn to current_directions
                 return directions, success
             else:
-                facing = self.rotate(self.rotate(facing)) # rotate 180
-                new_x, new_y = self.calculate_index(facing, x, y)
-                directions, success = self.solve_maze(facing, raw_maze, new_x, new_y)
+                facing = self.rotate(self.rotate(facing))  # rotate 180 degrees right
+                new_x, new_y = self.calculate_coords(facing, x, y) #next coordinates
+                directions, success = self.solve_maze(facing, raw_maze, new_x, new_y) #update vars
                 if success:
-                    current_directions.append(Directions.left) # 180 degrees + right = left
-                    directions.extend(current_directions)
+                    current_directions.append(Directions.left)  #right turn + 180 degrees = left turn
+                    directions.extend(current_directions) #add a left turn to current_directions
                     return directions, success
                 else:
-                    return [], False
+                    return [], False #this branch of the maze is unsolvable
 
-
-
-
-        # # move forward unless not possible
-        # try:
-        #     if self.solve_maze(raw_maze, x+1, y):
-        #         return True
-        #     if status == 'wall':
-        #         try:
-        #             self.solve_maze(raw_maze, x, y+1) # turn right
-        #         except IndexError:
-        #             pass
-
-
-        # # check each direction
-        # if x < len(raw_maze[y]) - 1 and self.solve_maze(raw_maze, x + 1, y):  # continue
-        #     self.output_label.setText(f"continue {current_directions[0]}")
-        #     return True
-        # elif x > 0 and self.solve_maze(raw_maze, x - 1, y):  # go back
-        #     self.output_label.setText(f"go {current_directions[1]}")
-        #     return True
-        # elif y < len(raw_maze) and self.solve_maze(raw_maze, x, y + 1):  # turn right
-        #     self.output_label.setText(f"turn {current_directions[2]}")
-        #     # shift the directions list left
-        #     current_directions[3] = current_directions[0]
-        #     current_directions[2] = current_directions[1]
-        #     current_directions[1] = current_directions[2]
-        #     current_directions[0] = current_directions[3]
-        #     return True
-        # elif y > 0 and self.solve_maze(raw_maze, x, y - 1):  # turn left
-        #     self.output_label.setText(f"turn {current_directions[3]}")
-        #     # shift the directions right by 1
-        #     current_directions[0] = current_directions[3]
-        #     current_directions[1] = current_directions[2]
-        #     current_directions[2] = current_directions[1]
-        #     current_directions[3] = current_directions[0]
-        #     return True
-        # return False
-
-    def calculate_index(self, facing:Facing, x:int, y:int,) -> tuple[int, int]:
+    def calculate_coords(self, facing: Facing, x: int, y: int, ) -> tuple[int, int]:
+        """
+        Calculates the coordinates of the direction being faced, ie next cell to check.
+        :param facing: direction being faced
+        :param x: current x position
+        :param y: current y position
+        :return: coordinates of the next cell to check
+        """
         match facing:
             case Facing.north:
-                return x-1, y
+                return x - 1, y #previous row, same col
             case Facing.south:
-                return x+1, y
+                return x + 1, y #next row, same col
             case Facing.east:
-                return x, y+1
+                return x, y + 1 #same row, next col
             case Facing.west:
-                return x, y-1
+                return x, y - 1 #same row, previous col
 
-    def rotate(self, facing:Facing, rotation:Directions=Directions.right) -> Facing:
-        return Facing((facing.value + rotation.value)%4)
-
-
-    #######################################################################################
-
-    # maybe do later
-    def colors(self):
-        pass
+    def rotate(self, facing: Facing, rotation: Directions = Directions.right) -> Facing:
+        """
+        Rotate the maze once by rotation. Default is to the right (clockwise).
+        :param facing: direction being faced
+        :param rotation: rotation direction (left or right)
+        :return: new direction being faced
+        """
+        return Facing((facing.value + rotation.value) % 4) #see Facing and Directions classes above
